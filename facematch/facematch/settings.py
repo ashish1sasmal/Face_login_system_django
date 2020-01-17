682c7e793473508871ffc2b112a09181f040f1e9
ashish1sasmal <ashishsasmal1@gmail.com>
2020-01-18 01:48:14 +0530

    wqad

--- facematch/facematch/settings.py
+++ facematch/facematch/settings.py
@@ -15,7 +15,6 @@ import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
